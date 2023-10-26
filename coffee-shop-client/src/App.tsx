08d736b40837bf16eff0e37025c4fe67b0f08138
Andrea Angelova <andrea.angelova@swisscom.com>
2023-10-26 20:22:59 +0200

    set up router

--- coffee-shop-client/src/App.tsx
+++ coffee-shop-client/src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './layouts/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* TODO: add not found page <Route path="*" element={<NoPage />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ coffee-shop-client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1>Choose your coffee</h1>
+  );
+};
+
+export default Home;
