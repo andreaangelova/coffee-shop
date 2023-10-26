f451c809852a7627d328b116d9d7f0301a525671
Andrea Angelova <andrea.angelova@swisscom.com>
2023-10-26 11:50:53 +0200

    set up theming and header

---
+++ coffee-shop-client/src/layouts/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="nav">
+        <div className="text-centered">
+            <h2>Coffee Shop</h2>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
